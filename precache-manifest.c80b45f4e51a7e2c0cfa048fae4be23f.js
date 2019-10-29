eb29e4b42eb6c2c96b0243cce559d0a20be2214d
Darek Sikorra <dariusz.sikorra@gmail.com>
2019-10-29 18:34:15 +0100

    Updates

---
+++ precache-manifest.c80b45f4e51a7e2c0cfa048fae4be23f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d81bbfa12885de5149b154804e5d95",
+    "url": "/TodoList-useHooks/index.html"
+  },
+  {
+    "revision": "97c67ab4fd66f1d7a986",
+    "url": "/TodoList-useHooks/static/css/main.e5ead448.chunk.css"
+  },
+  {
+    "revision": "0beb55a769809b383317",
+    "url": "/TodoList-useHooks/static/js/2.f6f81eca.chunk.js"
+  },
+  {
+    "revision": "97c67ab4fd66f1d7a986",
+    "url": "/TodoList-useHooks/static/js/main.28375628.chunk.js"
+  },
+  {
+    "revision": "712626d3c041e07408cc",
+    "url": "/TodoList-useHooks/static/js/runtime-main.59206572.js"
+  }
+]);
